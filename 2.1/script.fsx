de29194eee9d4e5081f46b51d3d081b7bf77b1f3
Martin Björkström <martin.bjorkstrom@gmail.com>
2022-03-10 23:25:22 +0200

    Solution for Exercise 2.1

---
+++ 2.1/script.fsx
@@ -0,0 +1,72 @@
+type Milestone = {
+    location : string
+    traveled : int
+    previous : Milestone option
+}
+
+let distances =
+    $"{__SOURCE_DIRECTORY__}/data.csv"
+    |> System.IO.File.ReadAllLines
+    |> Array.skip 1
+    |> Array.choose (fun str ->
+        match str.Split(',') with
+        | [|a;b;km|] -> Some [|(a, (b, km |> int)); (b, (a, km |> int))|]
+        | _ -> None)
+    |> Array.concat
+    |> Array.groupBy fst
+    |> Array.map (fun (key, arr) ->
+        (key, arr |> Array.map snd |> Array.distinct))
+    |> Map.ofArray
+
+let adjacent location =
+    distances.[location]
+
+let milestones milestone =
+    let rec loop milestone =
+        match milestone.previous with
+        | None -> [milestone.location]
+        | Some previous ->
+            milestone.location :: loop previous
+
+    loop milestone
+    |> List.rev
+
+
+let route start end' =
+    let queue = System.Collections.Generic.PriorityQueue<string, int>()
+    let visited = System.Collections.Generic.Dictionary<string, Milestone>()
+
+    visited.[start] <- { location = start; traveled = 0; previous = None }
+    queue.Enqueue (start, 0)
+
+    let rec loop () =
+        if queue.Count = 0 then ()
+        else
+            let current = queue.Dequeue()
+
+            current
+            |> adjacent
+            |> Array.iter (fun (next, distance) ->
+                let previous = visited.[current]
+                let traveled = previous.traveled + distance
+
+                let visitedDistance =
+                    match visited.TryGetValue(next) with
+                    | (true, milestone) -> milestone.traveled
+                    | _ -> System.Int32.MaxValue
+
+                if traveled < visitedDistance then
+                    visited.[next] <- { location = next; traveled = traveled; previous = Some(previous) }
+                    queue.Enqueue(next, traveled))
+            loop ()
+
+    loop ()
+
+    visited.[end']
+    |> milestones
+
+// Run
+route "Steamdrift" "Leverstorm"
+|> List.reduce (fun a b -> $"{a},{b}") 
+
+// "Steamdrift,Cogburg,Copperhold,Leverstorm"
